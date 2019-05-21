8f253516c895d13016de948979ffc7d8664823ca
Anna <anna.foerster1@gmx.net>
2019-05-21 20:18:48 +0200

    finished chapter 4

--- count.js
+++ count.js
@@ -13,4 +13,16 @@ But bears it out even to the edge of doom.
   If this be error and upon me proved,
   I never writ, nor no man ever loved.`;
 
-console.log(sonnet);
+let uniques = new Map();
+let words = sonnet.match(/[\w']+/g);
+
+for (let i = 0; i < words.length; i++) {
+	let word = words[i];
+	if (!!uniques.get(word)) { 
+		uniques.set(word, (uniques.get(word))+1);
+	} else {
+		uniques.set(word, 1);
+	}
+}
+
+console.log(uniques);
