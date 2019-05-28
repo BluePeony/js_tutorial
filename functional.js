e9d8ad9ec7c97830e5aece38af62f3e56cc67018
Anna Foerster <anna.foerster@elkb.de>
2019-05-28 13:07:52 +0200

    updated reduce

--- functional.js
+++ functional.js
@@ -44,11 +44,69 @@ function imperativeSingles(elements) {
 	return singles;
 }
 
-console.log(imperativeSingles(states));
+//console.log(imperativeSingles(states));
 
 //singles: functional version
 function functionalSingles(elements){
 	return elements.filter(element => element.split(/\s+/).length === 1);
 }
 
-console.log(functionalSingles(states));
+//console.log(functionalSingles(states));
+
+//return the Dakotas
+function imperativeDakota(elements) {
+	let dakotas = [];
+	elements.forEach(function(element){
+		if (element.includes("Dakota")) {
+			dakotas.push(element);
+		}
+	});
+	return dakotas;
+}
+
+//console.log(imperativeDakota(states));
+
+function functionalDakota(elements){
+	return elements.filter(element => element.includes("Dakota"));
+}
+
+//console.log(functionalDakota(states));
+
+let numbers = [1,2,3,4,5,6,7,8,9,10];
+
+//sum: Imperative solution
+function imperativeSum(elements){
+	let total = 0;
+	elements.forEach(function(n){
+		total += n;
+	})
+	return total;
+}
+
+//console.log(imperativeSum(numbers));
+
+//sum: Functional solution
+function functionalSum(elements){
+	return elements.reduce((total, n) => {return total += n;});
+}
+
+//console.log(functionalSum(numbers));
+
+//length: Imperative solution
+function imperativeLengths(elements){
+	let lengths = {};
+	elements.forEach(function(element){
+		lengths[element] = element.length;
+	});
+	return lengths;
+}
+
+console.log(imperativeLengths(states));
+
+//length: Functional solution
+function functionalLengths(elements) {
+	return elements.reduce((lengths, element) => {lengths[element] = element.length;
+																								return lengths}, {});
+}
+
+console.log(functionalLengths(states));
