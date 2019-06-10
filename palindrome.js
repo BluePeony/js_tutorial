dfaece7957c4dca4f182d3e95bc163f8790d859c
Anna <anna.foerster1@gmx.net>
2019-06-10 12:07:29 +0200

    Prevent Jekyll build

--- palindrome.js
+++ palindrome.js
@@ -1,8 +1,44 @@
-function reverse(string) {
-	return Array.from(string).reverse().join("");
+//Adds `reverse` to all string.
+String.prototype.reverse = function(){
+	return Array.from(this).reverse().join("");
 }
 
-function palindrome(string) {
-	let processedContent = string.toLowerCase();
-	return processedContent === reverse(processedContent);
+String.prototype.blank = function(){
+	return (this == "") || (!!this.match(/^\s+$/));
 }
+
+Array.prototype.last = function(){
+	return this[this.length-1];
+}
+
+//Defines a Phrase object.
+function Phrase(content) {
+	this.content = content;
+
+	this.processor = function(string){
+		return string.toLowerCase();
+	}
+
+	this.processedContent = function processedContent(){
+		return this.processor(this.content);
+	}
+
+	//Returns true of the phrase is a palindrome, false otherwise.
+	this.palindrome = function palindrome(){
+		return this.processedContent() === this.processedContent().reverse();
+	}
+
+}
+
+//Defines a TranslatedPhrase object.
+function TranslatedPhrase(content, translation){
+	this.content = content;
+	this.translation = translation;
+
+	//Returns translation processed for palindrome testing
+	this.processedContent = function processedContent(){
+		return this.processor(this.translation);
+	}
+}
+
+TranslatedPhrase.prototype = new Phrase();
